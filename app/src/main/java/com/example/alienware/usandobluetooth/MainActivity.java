e5830f2a92c77d597ec3c30c0f5400bfe3622a8b
Enrique Castillo <enrique.castillo.itcm@gmail.com>
2021-04-28 16:53:58 -0500

    Borro una linea

--- app/src/main/java/com/example/alienware/usandobluetooth/MainActivity.java
+++ app/src/main/java/com/example/alienware/usandobluetooth/MainActivity.java
@@ -81,7 +81,6 @@ public class MainActivity extends Activity {
         if(bAdapter.isEnabled()){
             btnBluetooth.setText("Desactivar");
             btnBuscarDispositivo.setEnabled(true);
-            Log.d("e","cambio")
         }
         else
             btnBluetooth.setText("Activar");
